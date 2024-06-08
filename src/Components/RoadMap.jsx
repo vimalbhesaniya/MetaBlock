05c596c07ca96b7969ec24eeb642e83e7a561188
vimalbhesaniya <vimalbhesaniya007@gmail.com>
2024-06-08 23:51:44 +0530

    roadmap created

---
+++ src/Components/RoadMap.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css'
+const RoadMap = () => {
+  return (
+        <img src="data:image/svg+xml,%3csvg%20width='24'%20height='920'%20viewBox='0%200%2024%20920'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3crect%20x='10'%20width='4'%20height='920'%20fill='url(%23paint0_linear_245_127)'/%3e%3ccircle%20cx='12'%20cy='124'%20r='10'%20fill='white'%20stroke='black'%20stroke-width='4'/%3e%3ccircle%20cx='12'%20cy='398'%20r='10'%20fill='white'%20stroke='black'%20stroke-width='4'/%3e%3ccircle%20cx='12'%20cy='535'%20r='10'%20fill='white'%20stroke='black'%20stroke-width='4'/%3e%3ccircle%20cx='12'%20cy='672'%20r='10'%20fill='white'%20stroke='black'%20stroke-width='4'/%3e%3ccircle%20cx='12'%20cy='809'%20r='10'%20fill='white'%20stroke='black'%20stroke-width='4'/%3e%3ccircle%20cx='12'%20cy='261'%20r='10'%20fill='white'%20stroke='black'%20stroke-width='4'/%3e%3cdefs%3e%3clinearGradient%20id='paint0_linear_245_127'%20x1='13.9569'%20y1='-4.52034e-06'%20x2='13.8604'%20y2='920'%20gradientUnits='userSpaceOnUse'%3e%3cstop%20stop-color='%23B0EAFD'/%3e%3cstop%20offset='0.546877'%20stop-color='%23C783FC'/%3e%3cstop%20offset='1'%20stop-color='%23F9FB32'/%3e%3c/linearGradient%3e%3c/defs%3e%3c/svg%3e" alt=""  className='timeLineImage'/>
+  
+  );
+}
+
+export default RoadMap;
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./home.css"
 import Button from '../../Components/Button'
 import cryptoImage from "../../assets/staking.png"
+import RoadMap from '../../Components/RoadMap'
 const Home = () => {
     return (
         <>
@@ -17,7 +18,7 @@ const Home = () => {
 
                 </div>
                 <video width="600" autoPlay loop muted className='introductionVideo'>
-                    <source src='Videos/ICON_VERSION8.mp4' type="video/mp4" />
+                    <source src='Videos/crypto.mp4' type="video/mp4" />
                 </video>
             </section>
 
@@ -39,7 +40,7 @@ const Home = () => {
 
             <section className='realitySection appContainer'>
                 <p className='realityTitle'>ENDLESS REALITY</p>
-                <p className='realityHeading'>DISCOVER THE OPPORTUNITIES OF <br /> METAVERSE</p>
+                <p className='realityHeading'>ECOSYSTEM</p>
             </section>
 
             <section className='appContainer stakeSection'>
@@ -48,42 +49,105 @@ const Home = () => {
                     <div className='bluryDiv1'></div>
                 </div>
                 <div className='stakeRight'>
-                    <h1 className='stakHeading'>DIGITAL UNIVERSE ARCADE</h1>
+                    <h1 className='stakHeading'>Staking</h1>
                     <div>
                         <h3 className='subHeading'>Alien Auras </h3>
-                        <p className='font subDesc'>Alien Auras is a cutting-edge font inspired by the dynamic world of cryptocurrency. Its futuristic design captures the essence of digital innovation and technological advancement, making it an ideal choice for crypto-related projects seeking a bold and forward-thinking aesthetic.</p>
+                        <p className='font subDesc'>Alien Auras is the dynamic world of cryptocurrency. Its futuristic design captures the essence of digital innovation and technological advancement for crypto-related projects seeking a bold and forward-thinking aesthetic.</p>
                     </div>
                     <div>
-    <h3 className='subHeading'>Flash Loan Arbitrage Bot</h3>
-    <p className='font subDesc'>Leverage Flash Loan Arbitrage Bot for rapid and lucrative trading by exploiting price variations across decentralized finance platforms.</p>
-</div>
+                        <h3 className='subHeading'>Flash Loan Arbitrage Bot</h3>
+                        <p className='font subDesc'>Leverage Flash Loan Arbitrage Bot for rapid and lucrative trading by exploiting price variations across decentralized finance platforms.</p>
+                    </div>
 
-<div>
-    <h3 className='subHeading'>NFT Staking</h3>
-    <p className='font subDesc'>Engage in NFT Staking to earn passive income and unlock rewards, enhancing the value and utility of your digital assets within the blockchain ecosystem.</p>
-</div>
+                    <div>
+                        <h3 className='subHeading'>NFT Staking</h3>
+                        <p className='font subDesc'>Engage in NFT Staking to earn passive income and unlock rewards, enhancing the value and utility of your digital assets within the blockchain ecosystem.</p>
+                    </div>
 
-<div>
-    <h3 className='subHeading'>Aura Finance</h3>
-    <p className='font subDesc'>Access Aura Finance for comprehensive financial solutions, including lending, borrowing, and yield farming, facilitated by cutting-edge blockchain technology.</p>
-</div>
+                    <div>
+                        <h3 className='subHeading'>Aura Finance</h3>
+                        <p className='font subDesc'>Access Aura Finance for comprehensive financial solutions, including lending, borrowing, and yield farming, facilitated by cutting-edge blockchain technology.</p>
+                    </div>
 
-<div>
-    <h3 className='subHeading'>Aura Wallet</h3>
-    <p className='font subDesc'>Securely manage your cryptocurrency assets with Aura Wallet, offering a user-friendly interface and advanced security features for convenient storage and transactions.</p>
-</div>
+                    <div>
+                        <h3 className='subHeading'>Aura Wallet</h3>
+                        <p className='font subDesc'>Securely manage your cryptocurrency assets with Aura Wallet, offering a user-friendly interface and advanced security features for convenient storage and transactions.</p>
+                    </div>
 
-<div>
-    <h3 className='subHeading'>Alien Aura Battle Game</h3>
-    <p className='font subDesc'>Experience thrilling gameplay in the Alien Aura Battle Game, where players engage in strategic battles and earn rewards using blockchain technology.</p>
-</div>
+                    <div>
+                        <h3 className='subHeading'>Alien Aura Battle Game</h3>
+                        <p className='font subDesc'>Experience thrilling gameplay in the Alien Aura Battle Game, where players engage in strategic battles and earn rewards using blockchain technology.</p>
+                    </div>
 
-<div>
-    <h3 className='subHeading'>Aura Chain</h3>
-    <p className='font subDesc'>Discover Aura Chain, a high-performance blockchain network designed for scalability, security, and interoperability, enabling seamless transactions and decentralized applications.</p>
-</div>
+                    <div>
+                        <h3 className='subHeading'>Aura Chain</h3>
+                        <p className='font subDesc'>Discover Aura Chain, a high-performance blockchain network designed for scalability, security, and interoperability, enabling seamless transactions and decentralized applications.</p>
+                    </div>
 
                 </div>
+            </section>
+
+            <section className='roadMapContainer appContainer'>
+                <div className='mapContainer'>
+                    <div className='phase1 costomMargin'>
+                        <div className='phaseLeft'>
+                            <p className='percentageLeft'>10%</p>
+                        </div>
+                        <div className='phaseRight'>
+                            <h1 className='paseTitle '>PHASE1</h1>
+                            <p className='aboutPhase'>Empowering Tokenization with Seamless Web 3 Connectivity</p>
+                        </div>
+                    </div>
+                    <div className='phase1'>
+                        <div className='phaseLeft'>
+                            <h1 className='paseTitle'>PHASE2</h1>
+                            <p className='aboutPhase text-right'>Secure, Stake, and Earn: Powering Your Metaverse Journey.</p>
+                        </div>
+                        <div className='phaseRight'>
+                            <p className='percentageRight'>30%</p>
+                            
+                        </div>
+                    </div>
+                    <div className='phase1'>
+                        <div className='phaseLeft'>
+                            <p className='percentageLeft'>50%</p>
+                        </div>
+                        <div className='phaseRight'>
+                            <h1 className='paseTitle '>PHASE3</h1>
+                            <p className='aboutPhase'>Step into the Future: AR/VR Casino Gaming Redefined.</p>
+                        </div>
+                    </div>
+                    <div className='phase1'>
+                        <div className='phaseLeft'>
+                            <h1 className='paseTitle'>PHASE4</h1>
+                            <p className='aboutPhase text-right'>Crafting Boundless Worlds: Your Complete Metaverse Game Ecosystem Awaits</p>
+                        </div>
+                        <div className='phaseRight'>
+                            <p className='percentageRight'>65%</p>
+                            
+                        </div>
+                    </div>
+                    <div className='phase1 '>
+                        <div className='phaseLeft'>
+                            <p className='percentageLeft'>80%</p>
+                        </div>
+                        <div className='phaseRight'>
+                            <h1 className='paseTitle '>PHASE5</h1>
+                            <p className='aboutPhase'>In house blockchain for HoriXverse</p>
+                        </div>
+                    </div>
+                    <div className='phase1'>
+                        <div className='phaseLeft'>
+                            <h1 className='paseTitle'>PHASE6</h1>
+                            <p className='aboutPhase text-right'>Coming soon on metaverse markets like metastore</p>
+                        </div>
+                        <div className='phaseRight'>
+                            <p className='percentageRight'>100%</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                    <RoadMap />
             </section>
 
         </>
